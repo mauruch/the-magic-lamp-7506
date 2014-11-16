875d6a3bab76cf75b13994c8ab76d30a56339809
capolupo.mauro <capolupo.mauro@gmail.com>
2014-11-16 21:21:28 +0000

    some utils method added in StringUtils

--- BillionWords/src/BillionWords.cpp
+++ BillionWords/src/BillionWords.cpp
@@ -2,21 +2,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include "StringUtils.h"
 using namespace std;
 
-int main () {
-  string line;
-  ifstream myfile ("names.txt");
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) )
-    {
-      cout << line << '\n';
-    }
-    myfile.close();
-  }
+int main() {
+	string line;
+	string sentence = "And_finally	18";
+	ifstream myfile("output.txt");
+	if (myfile.is_open()) {
+		while (getline(myfile, line)) {
+			StringUtils::removeSpaces(line);
+			StringUtils::removeSpaces(sentence);
+			if (sentence.compare(line) == 0) {
+				std::vector<string> array = StringUtils::split(line, '_');
+				cout << "tamaño: " << array.size() << endl;
+				cout << line << endl;
+				cout << "weight: " << StringUtils::getWeight(array) << endl;
+			}
+//			else cout << line << endl;
+		}
+		myfile.close();
+	}
 
-  else cout << "Unable to open file";
+	else
+		cout << "Unable to open file";
 
-  return 0;
+	return 0;
 }
---
+++ BillionWords/src/StringUtils.h
@@ -0,0 +1,43 @@
+/*
+ * StringUtils.h
+ *
+ *  Created on: 16/11/2014
+ *      Author: mauro
+ */
+
+#ifndef STRINGUTILS_H_
+#define STRINGUTILS_H_
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class StringUtils {
+
+public:
+
+	virtual ~StringUtils();
+
+	StringUtils();
+
+	static vector<string> &split(const string &s, char delim,
+			vector<string> &elems);
+
+	static vector<string> split(const string &s, char delim);
+
+	/**
+	 * Obtain the weight of n-gram
+	 */
+	static int getWeight(vector<string> &elems);
+
+	/**
+	 * Trim string, remove all whitespace
+	 */
+	static void removeSpaces(string& str);
+
+};
+
+#endif /* STRINGUTILS_H_ */
