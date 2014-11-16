390d0a288eacf18a95833fe96bc42c09c2afa616
capolupo.mauro@gmail.com <capolupo.mauro@gmail.com@240499ff-6c40-3cbb-8525-899ffe22591c>
2014-11-16 21:21:28 +0000

    some utils method added in StringUtils

---
+++ trunk/BillionWords/src/StringUtils.cpp
@@ -0,0 +1,62 @@
+/*
+ * StringUtils.cpp
+ *
+ *  Created on: 16/11/2014
+ *      Author: mauro
+ */
+
+#include "StringUtils.h"
+
+StringUtils::~StringUtils() {
+	// TODO Auto-generated destructor stub
+}
+
+StringUtils::StringUtils() {
+	// TODO Auto-generated constructor stub
+
+}
+
+vector<string> & StringUtils::split(const string &s, char delim,
+		vector<string> &elems) {
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+	return elems;
+
+}
+
+vector<string> StringUtils::split(const string &s, char delim) {
+	vector<std::string> elems;
+	split(s, delim, elems);
+	return elems;
+
+}
+
+int StringUtils::getWeight(vector<string> &elems) {
+	string lastElement = elems.at(elems.size() - 1);
+	char ch = *lastElement.rbegin();
+	return (int) ch - '0';
+}
+
+void StringUtils::removeSpaces(string& str) {
+	/* remove multiple spaces */
+	int k = 0;
+	for (int j = 0; j < str.size(); ++j) {
+		if ((str[j] != ' ') || (str[j] == ' ' && str[j + 1] != ' ')) {
+			str[k] = str[j];
+			++k;
+		}
+
+	}
+	str.resize(k);
+
+	/* remove space at the end */
+	if (str[k - 1] == ' ')
+		str.erase(str.end() - 1);
+	/* remove space at the begin */
+	if (str[0] == ' ')
+		str.erase(str.begin());
+}
+
