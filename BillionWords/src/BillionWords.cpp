69822ae1c3332c1f460e8a0ce4f1098204e38ce0
fdcancellara <fdcancellara@gmail.com>
2014-11-19 04:14:42 +0000

    Ahi modifique algo para ver si funciona o rompo todo Lo que puse es que ahora los archivos donde se tengan que trabajar que vengan como parametros a la hora de correrlos

--- BillionWords/src/BillionWords.cpp
+++ BillionWords/src/BillionWords.cpp
@@ -1,4 +1,5 @@
 // reading a text file
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,15 +8,23 @@
 #include "StringUtils.h"
 using namespace std;
 
-int main() {
+//El primer argumento es donde esta el archivo de unigramas
+//El segundo argumento es donde esta el archivo de bigramas
+//El tercer argunemento es donde esta el archivo de trigramas
+//El cuarto argumento va a ser el archivo de salida
+//Modificar estos parametros desde las propiedades de run
+int main(int argc, char *argv[]) {
 	string line;
-	string sentence = "And_finally	18";
-	ifstream myfile("output.txt");
+	string unigrams = argv[0];
+	string bigrams = argv[1];
+	string trigrams = argv[2];
+	string output = argv[3];
+	ifstream myfile(output.c_str());	//el c_str() es porque:the standard streams doesn't accept a standard string, only c-string! So pass the string using c_str():
 	if (myfile.is_open()) {
 		while (getline(myfile, line)) {
 			StringUtils::removeSpaces(line);
-			StringUtils::removeSpaces(sentence);
-			if (sentence.compare(line) == 0) {
+			StringUtils::removeSpaces(unigrams);
+			if (unigrams.compare(line) == 0) {
 				std::vector<string> array = StringUtils::split(line, '_');
 				cout << "tamaño: " << array.size() << endl;
 				cout << line << endl;
