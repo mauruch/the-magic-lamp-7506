986a233378b9b767261080ef3cb3de97e8bc8bb6
fdcancellara@gmail.com <fdcancellara@gmail.com@240499ff-6c40-3cbb-8525-899ffe22591c>
2014-11-26 03:13:49 +0000

    Ahí agrego como el inicio de lo que debería pasar

--- trunk/BillionWords/src/BillionWords.cpp
+++ trunk/BillionWords/src/BillionWords.cpp
@@ -9,45 +9,58 @@
 #include "../headers/NGram.h"
 using namespace std;
 
+//El primer argumento es donde esta el archivo de ngramas
+//El segundo argumento es donde esta el archivo a predecir el que se llama test_v2
+//Modificar estos parametros desde las propiedades de run
 int main(int argc, char *argv[]) {
-
-	/* ngram file */
 	string ngrams = argv[1];
-
-	/* file to predict */
 	string test_v2 = argv[2];
-
-	NGram *ngram = new NGram(ngrams);
-
-	ngram->getWeight("nueve diez");
+	std::ofstream resultadoFinal;
+	resultadoFinal.open("resultadoFinal.txt", std::ios_base::app);
 
 	ifstream fileNgrams(ngrams.c_str());
 	ifstream fileTest_v2(test_v2.c_str());
+//el c_str() es porque:the standard streams doesn't accept a standard string, only c-string! So pass the string using c_str():
 
-	if (fileNgrams.is_open() && fileTest_v2.is_open()) {
+	if (fileNgrams.is_open() && fileTest_v2.is_open()){
 		//Una vez que tengo los files cargados
 		//si llego aca es que checkee que ya se han abierto bien los archivos
-		string lineaTexto;
+			string lineaTexto;
+			//Hago esto una sola vez para ponerle el "id", "sentence"
+			getline(fileTest_v2, lineaTexto);
+			resultadoFinal << lineaTexto;
+			resultadoFinal << std::endl;
 
-//			while (getline(fileTest_v2, lineaTexto)) {
-//							StringUtils::removeSpaces(lineaTexto);
-////								std::vector<string> array = StringUtils::split(lineaTexto, '_');
-////								cout << "tamaño: " << array.size() << endl;
-////								cout << lineaTexto << endl;
-////								cout << "weight: " << StringUtils::getWeight(array) << endl;
-//
-//				//			else cout << line << endl;
-//
-//			}
+			while (getline(fileTest_v2, lineaTexto)) {
+				int iniciaFrase;
+//				int terminaFrase;
+				int largoFrase = lineaTexto.length();
+				string linea = lineaTexto;
+				string parsearFrase = "";
+				iniciaFrase = lineaTexto.find("\"");
+//				terminaFrase = lineaTexto.rfind("\"");
+				for (int contador=0 ; contador<iniciaFrase ; contador++ ){
+					resultadoFinal << lineaTexto[contador];
+				}
+				resultadoFinal << "\"";	//la parte inicial de las comillas
 
-	} else {
+				lineaTexto = lineaTexto.substr (iniciaFrase+1,(largoFrase -iniciaFrase-2));	//aca tengo la frase sin la primera ni la ultima comilla
+				resultadoFinal << lineaTexto;
+				resultadoFinal << "\"";	//la parte final de las comillas
+
+				resultadoFinal << std::endl;
+			}
+
+	}
+	else{
 		cout << "Problemas al abrir los archivos" << endl;
 		cout << "Probar viendo si las direcciones pasadas son correctas" << endl;
 
 		return ERROR_AL_ABRIR_ARCHIVO;
 	}
 
+
+	resultadoFinal.close();
 	return 0;
 }
-
 
