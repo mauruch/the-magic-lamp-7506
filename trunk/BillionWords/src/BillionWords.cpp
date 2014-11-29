145b2cd2aaa75174ade10257541615843db1de8c
nmouteda@gmail.com <nmouteda@gmail.com@240499ff-6c40-3cbb-8525-899ffe22591c>
2014-11-29 22:23:38 +0000

    commit 2

--- trunk/BillionWords/src/BillionWords.cpp
+++ trunk/BillionWords/src/BillionWords.cpp
@@ -1,52 +1,28 @@
-// reading a text file
+#include "../headers/CONSTANTES.h"
+#include "../headers/StringUtils.h"
+#include "../headers/NGram.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <sstream>
-#include "../headers/CONSTANTES.h"
-#include "../headers/StringUtils.h"
-#include "../headers/NGram.h"
+
 using namespace std;
 
 
 int main(int argc, char *argv[]) {
-	string ngrams = argv[1];	//direccion de donde se encuentra el archivo
-	string test_v2 = argv[2];	//direccion de donde se encuentra el archivo
+	ifstream fileTest_v2 ("test_v2.txt");
 
-	std::ofstream outputFile;									//abro un archivo de salida
 
-	ifstream fileNgrams(ngrams.c_str());
-	ifstream fileTest_v2(test_v2.c_str());
-	NGram *NGramFunctions = new NGram(ngrams);	//Inicializo el NGram con la direccion donde se encuentra el archivo de los Ngramas
-
-	if (fileNgrams.is_open() && fileTest_v2.is_open()){
-		//Una vez que tengo los files cargados
-		//si llego aca es que checkee que ya se han abierto bien los archivos
+	if (fileTest_v2.is_open()){
 			string lineOfText;
-			//Hago esto una sola vez para ponerle el "id", "sentence"
 			getline(fileTest_v2, lineOfText);
-			outputFile << lineOfText;
-			outputFile << std::endl;
-
 			while (getline(fileTest_v2, lineOfText)) {
-				//aca se procesaria el metodo
-				int startOfSentence;
-				int longOfSentence = lineOfText.length();
-				startOfSentence = lineOfText.find("\"");
-
-				for (int counter=0 ; counter<startOfSentence ; counter++ ){
-					outputFile << lineOfText[counter];
-				}
-				outputFile << "\"";	//la parte inicial de las comillas
-				lineOfText = lineOfText.substr (startOfSentence+1,(longOfSentence -startOfSentence-2));	//aca tengo la frase sin la primera ni la ultima comilla
-
-				NGramFunctions->fillTheMissingWord(&lineOfText);	//aca le agrego la palabra que le falta
-
-				outputFile << lineOfText;
-				outputFile << "\"";	//la parte final de las comillas
-
-				outputFile << std::endl;
+				vector<string> vectorLine = StringUtils::split(lineOfText, ' ');
+				unsigned int wordMissingPos = NGram::whereIsMissingTheWord(vectorLine);
+				string salida = vectorLine.at(wordMissingPos).append(" POSICION ");
+				cout << salida << endl;
+				cout << wordMissingPos << endl;
 			}
 
 	}
@@ -57,8 +33,6 @@ int main(int argc, char *argv[]) {
 		return ERROR_AL_ABRIR_ARCHIVO;
 	}
 
-
-	outputFile.close();
 	return 0;
 }
 
