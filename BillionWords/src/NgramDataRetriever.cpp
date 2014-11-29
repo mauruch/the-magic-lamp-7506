f8dc9b91b8dfd01ba87d898fdbbac779c140969f
capolupo.mauro <capolupo.mauro@gmail.com>
2014-11-29 18:11:55 +0000

    grep implementation

--- BillionWords/src/NgramDataRetriever.cpp
+++ BillionWords/src/NgramDataRetriever.cpp
@@ -6,39 +6,54 @@ NgramDataRetriever::NgramDataRetriever() {
 }
 
 std::string exec(string cmd) {
-    FILE* pipe = popen(cmd.c_str(), "r");
-    if (!pipe) return "ERROR";
-    char buffer[128];
-    std::string result = "";
-    while(!feof(pipe)) {
-    	if(fgets(buffer, 128, pipe) != NULL)
-    		result += buffer;
-    }
-    pclose(pipe);
-    return result;
-}
-
-string NgramDataRetriever::getAllUnigrams(){
-	string result = exec("grep ^[^[:space:]][  ] ngram.merged");
+	FILE* pipe = popen(cmd.c_str(), "r");
+	if (!pipe)
+		return "ERROR";
+	char buffer[128];
+	std::string result = "";
+	while (!feof(pipe)) {
+		if (fgets(buffer, 128, pipe) != NULL)
+			result += buffer;
+	}
+	pclose(pipe);
 	return result;
 }
 
-string NgramDataRetriever::getExactNgram(string ngram) {
-	return exec("grep ^" + ngram + "     ngram.merged");
+long NgramDataRetriever::getUnigramTotalWeight() {
+	return 768648884;
+}
+
+string NgramDataRetriever::getExactNgram(string ngram, int ngramIdentifier) {
+	string command = "grep '^";
+	command.append(ngram);
+	command.append("	' "); //<tab>'
+	string unigramFile;
+	switch (ngramIdentifier) {
+	case 1:
+		command.append("ngram_uni.merged");
+		break;
+	case 2:
+		command.append("ngram_bi.merged");
+		break;
+	case 3:
+		command.append("ngram_tri.merged");
+		break;
+	}
+	return exec(command);
 }
 
 string NgramDataRetriever::getAllTrigramsGivenABigram(string bigram) {
-	return exec("grep ^" + bigram + "  ngram.merged");
+	return exec("grep ^" + bigram + "  ngram_tri.merged");
 }
 
 string NgramDataRetriever::getAllBigramsGivenAUnigram(string unigram) {
-	return exec("grep ^" + unigram + "\s[^[:space:]]\+[     ] ngram.merged");
+	return exec("grep ^" + unigram + "\s'" + " ngram_bi.merged");
 }
 
 long NgramDataRetriever::getWeight(string ngramResult) {
 	long sumOfWeights = 0;
 	vector<string> lines = StringUtils::split(ngramResult, '\n');
-	for(int i=0; i < lines.size(); i++){
+	for (int i = 0; i < lines.size(); i++) {
 		long weigth = 0;
 		string weightString = StringUtils::split(lines[i], '\t')[1];
 		weigth = atoll(weightString.c_str());
