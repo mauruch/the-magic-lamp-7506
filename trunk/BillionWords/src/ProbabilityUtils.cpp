da6677db2ac22660ecb5e99e7283b23901e9811c
capolupo.mauro@gmail.com <capolupo.mauro@gmail.com@240499ff-6c40-3cbb-8525-899ffe22591c>
2014-12-07 19:58:37 +0000

    Super optimizacion: ya no hay iteraciones. Ya se calculan los pesos mientras se carga el modelo en memoria.

--- trunk/BillionWords/src/ProbabilityUtils.cpp
+++ trunk/BillionWords/src/ProbabilityUtils.cpp
@@ -1,39 +1,103 @@
 #include "../headers/ProbabilityUtils.h"
 
-ProbabilityUtils::ProbabilityUtils( NGram *nGram){
+string getFirstUnigram(ifstream& ngram_file, string& lineOfText);
+
+vector<string> getFirstBigram(ifstream& ngram_file, string& lineOfText);
+
+ProbabilityUtils::ProbabilityUtils(NGram *nGram) {
 	this->nGram = nGram;
 
-	cout << "Comenzando a poner el archivo " << NGRAMS_CLEANED << endl;
+	cout << "Masticando el conocimiento..." << endl;
 
-	ifstream ngram_file("ngrams_cleaned_up65");
+	ifstream ngram_file("ngrams_cleaned_up5");
 
 	if (ngram_file.is_open()) {
-		cout << "Masticando el conocimiento..." << endl;
 		string lineOfText;
+		int sumWeightGivenUni = 0;
+		int sumWeightGivenBigram = 0;
+
+		//get first unigram and back to top
+		string aux_uni = getFirstUnigram(ngram_file, lineOfText);
+
+		//get first bigram and back to top
+		vector<string> aux_bigram = getFirstBigram(ngram_file, lineOfText);
+		int lineCounter = 1;
 		while (getline(ngram_file, lineOfText)) {
 			StringUtils::replace(lineOfText, "\t", " ");
 			vector<string> vector = StringUtils::split(lineOfText, ' ');
 			string unigram = vector.at(0);
+			if (unigram != aux_uni) {
+				totalWeightGivenUni[StringUtils::hashCode(aux_uni)] =
+						sumWeightGivenUni;
+				aux_uni = unigram;
+				sumWeightGivenUni = 0;
+			}
+			//the 99
 			if (vector.size() == 2) {
 				unigrams[unigram] = vector.at(1);
+				//the cat 99
 			} else if (vector.size() == 3) {
+
 				long unigram_hashed = StringUtils::hashCode(unigram);
+
+				if (vector.at(1) != aux_bigram[1]) {
+					long previous_unigram_hashed = StringUtils::hashCode(
+							aux_bigram[0]);
+					totalWeightGivenBigram[StringUtils::hashCode(
+							StringUtils::ltos(previous_unigram_hashed)
+									+ aux_bigram[1])] = sumWeightGivenBigram;
+					aux_bigram = vector;
+					sumWeightGivenBigram = 0;
+				}
+
 				bigrams[unigram_hashed][vector.at(1)] = vector.at(2);
+				sumWeightGivenUni += atol(vector.at(2).c_str());
+				//the cat is 99
 			} else {
 				long unigram_hashed = StringUtils::hashCode(unigram);
 				string numberAsString = StringUtils::ltos(unigram_hashed);
 				long bigram_hashed = StringUtils::hashCode(
 						numberAsString + vector.at(1));
 				trigrams[bigram_hashed][vector.at(2)] = vector.at(3);
+				sumWeightGivenBigram += atol(vector.at(3).c_str());
 			}
+			cout << lineCounter << endl;
+			lineCounter++;
 		}
-		cout << "Conocimiento incorporado!!!" << endl;
+
 	}
 
 	else {
 		cout << "Problemas al abrir los archivos" << endl;
 	}
 
+	cout << "Conocimiento incorporado!!!" << endl;
+
+}
+
+string getFirstUnigram(ifstream& ngram_file, string& lineOfText) {
+	streampos firstPos = ngram_file.tellg();
+	getline(ngram_file, lineOfText);
+	StringUtils::replace(lineOfText, "\t", " ");
+	string aux_uni = StringUtils::split(lineOfText, ' ').at(0);
+	ngram_file.seekg(firstPos);
+	return aux_uni;
+}
+
+vector<string> getFirstBigram(ifstream& ngram_file, string& lineOfText) {
+	streampos firstPos = ngram_file.tellg();
+	vector<string> aux_bigram;
+	bool read = true;
+	while (read && getline(ngram_file, lineOfText)) {
+		StringUtils::replace(lineOfText, "\t", " ");
+		vector<string> vector = StringUtils::split(lineOfText, ' ');
+		if (vector.size() == 3) {
+			aux_bigram = vector;
+			read = false;
+		}
+	}
+	ngram_file.seekg(firstPos);
+	return aux_bigram;
 }
 
 float ProbabilityUtils::getUnigramProbability(string unigram) {
@@ -49,17 +113,9 @@ float ProbabilityUtils::getBigramProbability(vector<string> bigram) {
 
 	long uni_hashed = StringUtils::hashCode(bigram[0]);
 
-	map<string, string> uni_map = this->bigrams[uni_hashed];
-	typedef map<string, string>::iterator it_type;
+	float sumOfAllBigrams = this->totalWeightGivenUni[uni_hashed];
 
-	float sumOfAllBigrams = 0;
-	for (it_type iterator = uni_map.begin(); iterator != uni_map.end();
-			iterator++) {
-		sumOfAllBigrams += atoi(iterator->second.c_str());
-	}
-
-	float weightBigram = atoi(
-			this->bigrams[uni_hashed][bigram[1]].c_str());
+	float weightBigram = atoi(this->bigrams[uni_hashed][bigram[1]].c_str());
 
 	return (weightBigram / sumOfAllBigrams);
 
@@ -71,16 +127,9 @@ float ProbabilityUtils::getTrigramProbability(vector<string> trigram) {
 	long bi_hashed = StringUtils::hashCode(
 			StringUtils::ltos(uni_hashed) + trigram[1]);
 
-	map<string, string> bi_map = this->trigrams[bi_hashed];
-	typedef map<string, string>::iterator it_type;
-	float sumOfAllTrigrams = 0;
-	for (it_type iterator = bi_map.begin(); iterator != bi_map.end();
-			iterator++) {
-		sumOfAllTrigrams += atoi(iterator->second.c_str());
-	}
+	float sumOfAllTrigrams = this->totalWeightGivenBigram[bi_hashed];
 
-	float weightTrigram = atoi(
-			this->trigrams[bi_hashed][trigram[2]].c_str());
+	float weightTrigram = atoi(this->trigrams[bi_hashed][trigram[2]].c_str());
 
 	return (weightTrigram / sumOfAllTrigrams);
 
@@ -106,10 +155,10 @@ float ProbabilityUtils::getWordProbability(vector<string> line,
 
 	if (wordPosition > 1) {
 		ngramExpression = this->nGram->getNgramExp(line, wordPosition,
-				TRIGRAM_EXPRESSION);
+		TRIGRAM_EXPRESSION);
 	} else {
 		ngramExpression = this->nGram->getNgramExp(line, wordPosition,
-				BIGRAM_EXPRESSION);
+		BIGRAM_EXPRESSION);
 	}
 
 	vector<string> ngram_splitted = StringUtils::split(ngramExpression, ' ');
